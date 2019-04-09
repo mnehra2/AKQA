f83bdeb59ae9b50f879e58654280c01a6bedb733
Meenakshi Nehra <meenakshi@guerrilla.com.au>
2019-04-10 00:48:58 +1000

    test update

--- AKQA.Feature.ConvertNumberToWords.Tests/Repositories/NumberToWordsRepositoryTest.cs
+++ AKQA.Feature.ConvertNumberToWords.Tests/Repositories/NumberToWordsRepositoryTest.cs
@@ -22,5 +22,20 @@ namespace AKQA.Feature.ConvertNumberToWords.Tests.Repositories
             stringInWords.Should().Be("ONE HUNDRED  FORTY-FOUR DOLLARS AND TWENTY CENTS");
 
         }
+
+        [TestMethod]
+        public void GetWordsFromAmount_ReturnsInCorrectStringInWords()
+        {
+            // Arrange         
+            decimal amount = 144.20M;
+
+            //Act
+            var model = new NumberToWordsRepository();
+            var stringInWords = model.GetWordsFromAmount(amount);
+
+            // Assert
+            stringInWords.Should().NotBe("ONE HUNDRED  FORTY-FOUR DOLLARS AND THIRTY CENTS");
+
+        }
     }
 }
